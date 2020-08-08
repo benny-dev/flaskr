d62a5631f154c993095e7d6fba7d3bb7caa9e02c
cudicudi <benjaminasante191@gmail.com>
2020-08-08 14:02:13 +0100

    error fixed

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -47,9 +47,9 @@ def create():
 def get_post(id, check_author=True):
         post = get_db().execute(
             'SELECT p.id, title, body, created, author_id, username'
-            'FROM post p JOIN user u ON p.author_id =u.id'
-            'WHERE p.id = ?',
-            (id),
+            ' FROM post p JOIN user u ON p.author_id = u.id'
+            ' WHERE p.id = ?',
+            (id,)
         ).fetchone()
         
         if post is None:
--- flaskr/db.py
+++ flaskr/db.py
@@ -12,7 +12,7 @@ def get_db():
         )
         g.db.row_factory = sqlite3.Row
         
-        return g.db
+    return g.db
 
 def close_db(e=None):
     db = g.pop('db', None)
